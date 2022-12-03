7486b766cb719e440c5709b76bd25b86dae4bc7d
denchik <andrey2008@inbox.ru>
2022-12-03 10:39:48 +0300

    DZ_1

---
+++ Home/DZ_1/dz_1.3/Program.cs
@@ -0,0 +1,12 @@
+﻿// Напишите программу, которая на вход принимает число и выдает, является ли число 
+// четным (делится ли оно на два без остатка).
+
+Console.WriteLine("Введите число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+
+if (a < 0) a = 0 +(-a);
+
+int ost = a % 2;
+
+if (ost > 0) Console.WriteLine("Число не четное");
+else Console.WriteLine("Число четное");
