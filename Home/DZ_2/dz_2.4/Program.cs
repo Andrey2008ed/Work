434fa949f7333cf5bd1845ac6458b9147b128549
denchik <andrey2008@inbox.ru>
2022-12-04 23:31:31 +0300

    DZ_2

---
+++ Home/DZ_2/dz_2.4/Program.cs
@@ -0,0 +1,11 @@
+﻿string DayNumber(int num)
+{
+    if (num > 5)
+        return $"{num} выходной";
+    else    
+        return $"{num} рабочий день";
+}   
+
+Console.WriteLine("Введите день недели:");
+int number = int.Parse(Console.ReadLine());
+Console.WriteLine(DayNumber(number));
